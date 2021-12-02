5054e70f81beab26687f00c458623a021bcf1800
kumarmo2 <kr.mohit6794@gmail.com>
2021-12-03 03:43:47 +0530

    made some changes.

--- src/main.rs
+++ src/main.rs
@@ -7,32 +7,21 @@ fn main() -> io::Result<()> {
     println!("Hello, world!");
     let content = fs::read_to_string(MEM_INFO_PATH)?;
 
-    let lines: Vec<&str> = content.split("\n").take(3).collect();
+    let lines: Vec<_> = content.split("\n").take(3).collect();
     let n = lines.len();
     if n < 3 {
         panic!("not correct file format");
     }
-
     let total_mem_line = lines[0];
-    let items: Vec<&str> = total_mem_line.split(":").map(|a| a.trim()).collect();
-    let total_mem = items[1];
+    let mut items = total_mem_line.split(":").map(|a| a.trim());
+    let total_mem = items.nth(1).unwrap();
     println!("total mem: {}", total_mem);
 
     let free_mem_line = lines[2];
-    let items: Vec<&str> = free_mem_line.split(":").map(|a| a.trim()).collect();
-    let free_mem = items[1];
-    println!("free mem: {}", free_mem);
+    let mut items = free_mem_line.split(":").map(|a| a.trim());
 
-    /*
-    for (index, line) in lines.enumerate() {
-        println!("line_no: {} starts", index);
-        let items: Vec<&str> = line.split(":").map(|a| a.trim()).collect();
-        for item in items {
-            println!("item: {}", item);
-        }
-        println!("line_no: {} ends", index);
-    }
-    */
+    let free_mem = items.nth(1).unwrap();
+    println!("free mem: {}", free_mem);
 
     io::Result::Ok(())
 }
